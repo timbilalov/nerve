f31d5ac4bc1a56e008f93eb91eaa5c771b64d4a1
Yarkeev Denis <d.yarkeev@corp.mail.ru>
2017-08-13 02:05:43 +0300

    iteration

--- typings/libs/axios.d.ts
+++ typings/libs/axios.d.ts
@@ -1,7 +1,6 @@
 declare module 'axios' {
     import Axios from 'node_modules/axios/index';
 
-    // const axios = Axios;
     export default Axios;
 
     export {AxiosResponse} from 'node_modules/axios/index';
